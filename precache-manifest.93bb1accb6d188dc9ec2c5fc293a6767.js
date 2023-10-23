c47893efa5e7b73c19bdd12442449c1039b5e588
Anjana Soja <anjanaedwin@gmail.com>
2023-10-23 21:40:34 +0100

    Updates

---
+++ precache-manifest.93bb1accb6d188dc9ec2c5fc293a6767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a74c6937f6aac85f3af6",
+    "url": "/Netflix/static/css/main.9aaecb35.chunk.css"
+  },
+  {
+    "revision": "a74c6937f6aac85f3af6",
+    "url": "/Netflix/static/js/main.eb7e7164.chunk.js"
+  },
+  {
+    "revision": "403c434e8f272edbb673",
+    "url": "/Netflix/static/js/runtime~main.c07c80b2.js"
+  },
+  {
+    "revision": "907cd7974811d756a02f",
+    "url": "/Netflix/static/js/2.991583f9.chunk.js"
+  },
+  {
+    "revision": "5d94e88e76c901d3478c2b364be6b633",
+    "url": "/Netflix/index.html"
+  }
+];
